a63e637efa34252c1634721201af5bddb7046a69
sapsari <sapsari@gmail.com>
2019-07-02 21:30:07 +0300

    3. Writing to registry - Writing

--- YellowExtension/YellowExtension/YellowOptionsPage.cs
+++ YellowExtension/YellowExtension/YellowOptionsPage.cs
@@ -9,6 +9,12 @@ namespace YellowNamespace
     [DesignerCategory("code")] // to hide designer
     class YellowOptionsPage : DialogPage
     {
+        /// <summary>
+        /// Where extension options are stored in the registry, can be get from base property SharedSettingsStorePath
+        /// </summary>
+        const string registryCollectionPath = @"ApplicationPrivateSettings\YellowNamespace\YellowOptionsPage";
+        const string propertyName = nameof(IsDisplayingYellowCommand) + "Raw";
+
         [Category("UI")]
         [DisplayName("Display command")]
         [Description("Display or hide extension shortcut in Tools menu")]
@@ -19,10 +25,25 @@ namespace YellowNamespace
         {
             base.SaveSettingsToStorage();
 
+            // overridden base method is not async, so run our own async method with ThreadHelper
+            ThreadHelper.JoinableTaskFactory.Run(async delegate
+            {
+                await SaveSettingsToStorageAuxAsync();
+            });
+        }
+
+        async Task SaveSettingsToStorageAuxAsync()
+        {
             // Retrieve the command and change its visibility
             OleMenuCommandService commandService = this.GetService(typeof(System.ComponentModel.Design.IMenuCommandService)) as OleMenuCommandService;
             var yellowCommand = commandService.FindCommand(new System.ComponentModel.Design.CommandID(YellowCommand.CommandSet, YellowCommand.CommandId));
             yellowCommand.Visible = IsDisplayingYellowCommand;
+
+            // Write custom property to the registry
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            var settingsManager = new ShellSettingsManager(ServiceProvider.GlobalProvider);
+            var userSettingsStore = settingsManager.GetWritableSettingsStore(SettingsScope.UserSettings);
+            userSettingsStore.SetBoolean(registryCollectionPath, propertyName, IsDisplayingYellowCommand);
         }
     }
 }
